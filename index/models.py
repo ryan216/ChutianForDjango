e982d2d0f65d458cedc143aee11af074cd4e7017
ryan <2486878020@qq.com>
2022-11-01 09:34:21 +0800

    11.1

--- index/models.py
+++ index/models.py
@@ -4,27 +4,28 @@ from django.db import models
 from django.db import models
 
 
-
 class page1(models.Model):
-    about= models.CharField(max_length=500,default='',null=True,blank=True)
-    nums_1=models.CharField(max_length=20,default='',null=True,blank=True)
-    nums_2=models.CharField(max_length=20,default='',null=True,blank=True)
-    nums_3=models.CharField(max_length=20,default='',null=True,blank=True)
-    nums_4=models.CharField(max_length=20,default='',null=True,blank=True)
+    about = models.TextField(default='', null=True, blank=True)
+    nums_1 = models.CharField(max_length=100,default='', null=True, blank=True)
+    nums_2 = models.CharField(max_length=100,default='', null=True, blank=True)
+    nums_3 = models.CharField(max_length=100,default='', null=True, blank=True)
+    nums_4 = models.CharField(max_length=100,default='', null=True, blank=True)
 
-    service1= models.CharField(max_length=500,default='',null=True,blank=True)
-    service2= models.CharField(max_length=500,default='',null=True,blank=True)
-    service3= models.CharField(max_length=500,default='',null=True,blank=True)
+    service1 = models.TextField(default='', null=True, blank=True)
+    service2 = models.TextField(default='', null=True, blank=True)
+    service3 = models.TextField(default='', null=True, blank=True)
 
-    innovation1= models.CharField(max_length=500,default='',null=True,blank=True)
-    innovation2= models.CharField(max_length=500,default='',null=True,blank=True)
-    innovation3= models.CharField(max_length=500,default='',null=True,blank=True)
+    innovation1 = models.TextField(default='', null=True, blank=True)
+    innovation2 = models.TextField(default='', null=True, blank=True)
+    innovation3 = models.TextField(default='', null=True, blank=True)
 
-    work1= models.CharField(max_length=500,default='',null=True,blank=True)
-    work2= models.CharField(max_length=500,default='',null=True,blank=True)
-    work3= models.CharField(max_length=500,default='',null=True,blank=True)
-
-
+    work1 = models.TextField(default='', null=True, blank=True)
+    work2 = models.TextField(default='', null=True, blank=True)
+    work3 = models.TextField(default='', null=True, blank=True)
 
 
-
+class news(models.Model):
+    title = models.CharField(max_length=100, default='', null=True, blank=True)
+    content = models.TextField(default='', null=True, blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    change_time = models.DateTimeField()
